793b99c6298adb17f71065969ec0caa27e791e88
jhcode33 <jhcode33@gmail.com>
2023-08-31 08:54:09 +0900

    Init: Add num9498/If
    - 9498번 If문 문제 풀이를 작성했습니다
    - BufferedReader을 사용하여 기존보다 속도를 높였습니다

--- src/level_02/num1330/If.java
+++ src/level_02/num1330/If.java
@@ -2,10 +2,9 @@ package level_02.num1330;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Scanner;
 import java.util.StringTokenizer;
 
-public class Main {
+public class If {
     public static void main(String[] args) throws Exception {
 //        Scanner sc = new Scanner(System.in);
 //
