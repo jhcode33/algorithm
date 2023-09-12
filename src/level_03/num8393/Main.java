31ce6b944ea050f34358c6b0856ee197150c8d89
jhcode33 <jhcode33@gmail.com>
2023-09-12 23:21:34 +0900

    Feat: Add num8393 합
    - 백준 알고리즘 8393번 합 풀이를 추가했습니다

---
+++ src/level_03/num8393/Main.java
@@ -0,0 +1,22 @@
+package level_03.num8393;
+
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int num = Integer.parseInt(st.nextToken());
+        int result = num;
+
+        for (int i = 1; i < num; i++) {
+            result += i;
+        }
+        System.out.print(result);
+
+    }
+}
