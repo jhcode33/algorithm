dcd7edbb0c92e242c476b05f5dbcd1aee9cd80e1
jhcode33 <jhcode33@gmail.com>
2023-09-14 00:23:46 +0900

    Feat: Add num25304 영수증
    - 백준 알고리즘 25304번 영수증 풀이를 추가했습니다

---
+++ src/level_03/num25304/Main.java
@@ -0,0 +1,25 @@
+package level_03.num25304;
+
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int total = Integer.parseInt(br.readLine());
+        int sort = Integer.parseInt(br.readLine());
+        int sum = 0;
+
+        for (int i = 0; i < sort; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int price = Integer.parseInt(st.nextToken());
+            int num = Integer.parseInt(st.nextToken());
+            sum += price * num;
+        }
+
+        System.out.print((total == sum) ? "Yes" : "No");
+    }
+}
