4fc9749b8209e74f7139d133fa3b96945da36483
jhcode33 <jhcode33@gmail.com>
2023-09-14 01:22:42 +0900

    Feat: Add num15552 빠른 A+B
    - 백준 알고리즘 15552번 문제 풀이를 추가했습니다
    - BufferedReader과 BufferedWriter를 쓰는 것이 핵심입니다

---
+++ src/level_03/num15552/Main.java
@@ -0,0 +1,22 @@
+package level_03.num15552;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int count = Integer.parseInt(br.readLine());
+
+        StringTokenizer st;
+        for (int i = 0; i < count; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            bw.write((a + b)+"\n");
+        }
+        bw.flush();
+        bw.close();
+    }
+}
