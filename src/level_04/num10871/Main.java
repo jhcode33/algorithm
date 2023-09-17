034453ab390364f978f4e8949ab2509620a67692
jhcode33 <jhcode33@gmail.com>
2023-09-17 23:49:53 +0900

    Feat: Add num10871 X보다 작은 수
    - 백준 알고리즘 10871번 X보다 작은 수 풀이를 추가합니다

---
+++ src/level_04/num10871/Main.java
@@ -0,0 +1,30 @@
+package level_04.num10871;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int size = Integer.parseInt(st.nextToken());
+        int num = Integer.parseInt(st.nextToken());
+        int[] list = new int[size];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < list.length; i++) {
+            list[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int j = 0; j < size; j++) {
+            if (list[j] < num) {
+                bw.write(list[j] + " ");
+            }
+        }
+        bw.flush();
+        bw.close();
+    }
+}
