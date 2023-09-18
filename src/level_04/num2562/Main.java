066aedcecb2d38b7f7a044d46f1cf60d2b51a22b
jhcode33 <jhcode33@gmail.com>
2023-09-19 00:19:33 +0900

    Feat: Add num2562 최대값
    - 백준 알고리즘 2562번 최대값 풀이를 추가했습니다

---
+++ src/level_04/num2562/Main.java
@@ -0,0 +1,27 @@
+package level_04.num2562;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int[] result = new int[9];
+        int max = Integer.MIN_VALUE;
+        int index = 0;
+
+        for (int i = 0; i < 9; i++) {
+            int num = Integer.parseInt(br.readLine());
+            result[i] = num;
+            if (num > max) {
+                max = num;
+                index = i + 1;
+            }
+        }
+
+        bw.write(max + "\n" + index);
+        bw.flush();
+        bw.close();
+    }
+}
