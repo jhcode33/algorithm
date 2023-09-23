b2868a489d0e530adceca02e8315c3cc5112ccf2
jhcode33 <jhcode33@gmail.com>
2023-09-23 14:32:36 +0900

    Feat: Add num27866 문자와 문자열
    - 백준 알고리즘 '27866 문자와 문자열' 풀이를 추가했습니다

---
+++ src/level_05/num27866/Main.java
@@ -0,0 +1,19 @@
+package level_05.num27866;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String S = br.readLine();
+        char[] charS = S.toCharArray();
+
+        int num = Integer.parseInt(br.readLine());
+
+        bw.write(charS[num -1]);
+        bw.flush();
+        bw.close();
+    }
+}
