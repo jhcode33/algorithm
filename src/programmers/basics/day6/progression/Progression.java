db003ff1444fa4da9ed73f2d95aff6f4dd489eed
jhcode33 <jhcode33@gmail.com>
2023-12-03 09:12:43 +0900

    Feat: Add Progression

---
+++ src/programmers/basics/day6/progression/Progression.java
@@ -0,0 +1,23 @@
+package programmers.basics.day6.progression;
+
+public class Progression {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+
+        for (int[] ints : queries) {
+            int n = ints[0];
+            int m = ints[1];
+
+            int temp = arr[n];
+            arr[n] = arr[m];
+            arr[m] = temp;
+        }
+
+        return arr;
+    }
+}
