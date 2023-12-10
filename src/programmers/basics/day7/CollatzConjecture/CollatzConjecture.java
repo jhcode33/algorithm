d0857d639457edc982b320e2ec4d60e75ae7bcf1
jhcode33 <jhcode33@gmail.com>
2023-12-10 18:19:14 +0900

    Feat: Add CollatzConjecture

---
+++ src/programmers/basics/day7/CollatzConjecture/CollatzConjecture.java
@@ -0,0 +1,40 @@
+package programmers.basics.day7.CollatzConjecture;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CollatzConjecture {
+    public static void main(String[] args) {
+        int n = 10;
+        Solution solution = new Solution();
+        int[] result = solution.solution(n);
+
+        for (int t : result) {
+            System.out.print(t + ", ");
+        }
+    }
+}
+
+class Solution {
+    public int[] solution(int n) {
+        List<Integer> list = new ArrayList<>();
+        list.add(n);
+        while(n != 1) {
+            if (n % 2 == 0) {
+                n = n / 2;
+                list.add(n);
+                continue;
+            }
+
+            if (n % 2 != 0) {
+                n = n * 3 + 1;
+                list.add(n);
+            }
+        }
+
+        return list.stream()
+                .mapToInt(Integer::intValue)
+                .toArray();
+    }
+}
