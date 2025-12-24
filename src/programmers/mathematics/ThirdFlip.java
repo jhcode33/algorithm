0858eb84b2467b5290b57d986811dd4349285af7
jhcode33 <jhcode33@gmail.com>
2025-12-24 14:40:46 +0900

    feat(mathematics): Add ThirdFilp with SpringBuilder and Integer.parseInt(string s, int radix)

---
+++ src/programmers/mathematics/ThirdFlip.java
@@ -0,0 +1,38 @@
+package programmers.mathematics;
+/*문제 설명
+자연수 n이 매개변수로 주어집니다. n을 3진법 상에서 앞뒤로 뒤집은 후, 이를 다시 10진법으로 표현한 수를 return 하도록 solution 함수를 완성해주세요.
+
+제한사항
+n은 1 이상 100,000,000 이하인 자연수입니다.
+입출력 예
+n	result
+45	7
+125	229
+
+입출력 예 설명
+입출력 예 #1
+
+답을 도출하는 과정은 다음과 같습니다.
+n (10진법)	n (3진법)	앞뒤 반전(3진법)	10진법으로 표현
+45	1200	0021	7
+따라서 7을 return 해야 합니다.
+
+입출력 예 #2
+답을 도출하는 과정은 다음과 같습니다.
+n (10진법)	n (3진법)	앞뒤 반전(3진법)	10진법으로 표현
+125	11122	22111	229
+따라서 229를 return 해야 합니다.
+*/
+
+public class ThirdFlip {
+    public int solution(int n) {
+        StringBuilder answer = new StringBuilder();
+
+        while (n > 0) {
+            answer.append(n % 3); // 나머지 순서를 그대로 저장하므로, 이미 뒤집기된 결과가 나온다.
+            n /= 3;
+        }
+
+        return Integer.parseInt(answer.toString(), 3);
+    }
+}
