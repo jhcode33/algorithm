0c6358abbfdbeb40c86887b5ce16e44e60c230d5
jhcode33 <jhcode33@gmail.com>
2026-04-17 17:42:34 +0900

    [level 0] Title: 정수 찾기, Time: 1.20 ms, Memory: 83.8 MB -BaekjoonHub

--- 프로그래머스/0/181840. 정수 찾기/정수 찾기.java
+++ 프로그래머스/0/181840. 정수 찾기/정수 찾기.java
@@ -1,10 +1,30 @@
+import java.util.*;
+
 class Solution {
     public int solution(int[] num_list, int n) {
-        for (int a : num_list) {
-            if (a == n) {
-                return 1;
+        int answer = 0;
+
+        Arrays.sort(num_list);
+        int low = 0;
+        int high = num_list.length -1;
+        int mid;
+
+        while(low <= high){
+            mid = (low+high) / 2;
+
+            if(num_list[mid] ==n){
+                answer = 1;
+                return answer;
+            }
+
+            else if(num_list[mid] > n){
+                high = mid-1;
+            }
+            else{
+                low = mid +1;
             }
         }
-        return 0;
+        answer = 0;
+        return answer;
     }
 }
