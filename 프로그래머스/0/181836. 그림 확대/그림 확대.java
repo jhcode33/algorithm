5c8ba7cea1e9cb9769097deaf139124d4fea3f6d
jhcode33 <jhcode33@gmail.com>
2026-04-27 23:54:43 +0900

    [level 0] Title: 그림 확대, Time: 6.78 ms, Memory: 90.7 MB -BaekjoonHub

--- 프로그래머스/0/181836. 그림 확대/그림 확대.java
+++ 프로그래머스/0/181836. 그림 확대/그림 확대.java
@@ -1,21 +1,17 @@
 import java.util.*;
+import java.util.stream.*;
 
 class Solution {
     public String[] solution(String[] picture, int k) {
         List<String> result = new ArrayList<>();
-        for (String pic : picture) {
-            for (int i = 0; i < k; i++) {
-                StringBuilder sb = new StringBuilder();
-                char[] s = pic.toCharArray();
-                for (int j = 0; j < s.length; j++) {
-                    for (int l = 0; l < k; l++) {
-                        sb.append(s[j]);
-                    }
-                }
-                result.add(sb.toString());
-            }
+        for (String row : picture) {
+            // 각 문자를 k번 반복한 문자열
+            String expanded = Arrays.stream(row.split(""))
+                                    .map(c -> c.repeat(k))
+                                    .collect(Collectors.joining());
+            // 해당 행을 k번 추가
+            for (int i = 0; i < k; i++) result.add(expanded);
         }
-
         return result.toArray(new String[0]);
     }
 }
